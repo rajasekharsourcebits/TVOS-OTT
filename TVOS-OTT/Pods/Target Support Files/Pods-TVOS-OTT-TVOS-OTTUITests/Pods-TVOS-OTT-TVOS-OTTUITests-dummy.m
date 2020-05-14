2fb063eec6267921ea86ffb92cefe4f70e1e5c60
Souvik <souvik.maiti@sourcebits.com>
2020-05-14 19:04:52 +0530

    Project initial setup with Folder Structure.

---
+++ TVOS-OTT/Pods/Target Support Files/Pods-TVOS-OTT-TVOS-OTTUITests/Pods-TVOS-OTT-TVOS-OTTUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TVOS_OTT_TVOS_OTTUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TVOS_OTT_TVOS_OTTUITests
+@end
---
+++ TVOS-OTT/TVOS-OTT/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TVOS-OTT
+//
+//  Created by Souvik on 14/05/20.
+//  Copyright © 2020 Sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
