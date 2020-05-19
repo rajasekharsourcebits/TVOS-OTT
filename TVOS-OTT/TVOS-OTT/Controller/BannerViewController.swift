c9a20e22910081c5dcf135293c52a028adc6c8ba
Kiran-Venkateshaiah <Kiran.Venkateshaiah@sourcebits.com>
2020-05-19 18:40:44 +0530

    Banner related changes. Added Collection view, Table view, Model for Banner.

---
+++ TVOS-OTT/TVOS-OTT/Controller/BannerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  BannerViewController.swift
+//  TVOS-OTT
+//
+//  Created by Kiran on 18/05/20.
+//  Copyright © 2020 Sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class BannerViewController: UIViewController {
+
+    @IBOutlet weak var bannerCollectioView: UICollectionView!
+    @IBOutlet weak var bannerPageControl: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
