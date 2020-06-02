0ef31ec9d9d3fce1f57b596bb5a950c5f65668aa
Souvik <souvik.maiti@sourcebits.com>
2020-06-02 11:49:04 +0530

    Favourite Screen Design

---
+++ TVOS-OTT/TVOS-OTT/Cell/CollectionCell/FavouriteCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  FavouriteCollectionCell.swift
+//  TVOS-OTT
+//
+//  Created by Souvik on 01/06/20.
+//  Copyright © 2020 Sourcebits. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteCollectionCell: UICollectionViewCell {
+    
+}
--- TVOS-OTT/TVOS-OTT/Controller/TabsItems/FavouritesViewController.swift
+++ TVOS-OTT/TVOS-OTT/Controller/TabsItems/FavouritesViewController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class FavouritesViewController: UIViewController {
+    
+    //Property
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var removeBtn: UIButton!
+    
+    var focusGuide = UIFocusGuide()
+    var preferredFocusView: UIView?
+    
+//    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+//        return [playBtn]
+//    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+//                guard let watchNow = view.watchNowButton,
+//            let transparentView = bannerView.transparentView else { return }
 
+        focusGuide.preferredFocusEnvironments = [playBtn]
+
+        view.addLayoutGuide(focusGuide)
+
+        focusGuide.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        focusGuide.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        focusGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        focusGuide.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+    }
 
     /*
     // MARK: - Navigation
@@ -27,4 +49,82 @@ class FavouritesViewController: UIViewController {
     }
     */
 
+}
+
+extension FavouritesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavouriteCollectionCell", for: indexPath) as? FavouriteCollectionCell {
+            cell.layer.cornerRadius = 10
+            cell.clipsToBounds = true
+            return cell
+        } else {
+            return FavouriteCollectionCell()
+        }
+    }
+    
+    
+}
+
+extension FavouritesViewController {
+    
+    fileprivate func setNextFocusUI(_ context: UIFocusUpdateContext) {
+        context.nextFocusedView?.layer.shadowColor = UIColor.lightGray.cgColor
+        context.nextFocusedView?.layer.shadowOpacity = 1
+        context.nextFocusedView?.layer.shadowOffset = CGSize.zero
+        context.nextFocusedView?.layer.shadowRadius = 5
+       
+        context.nextFocusedView?.transform = CGAffineTransform.identity.scaledBy(x: 1.1, y: 1.1)
+    }
+    
+    fileprivate func setPrevioulyFocusedUI(_ context: UIFocusUpdateContext) {
+        context.previouslyFocusedView?.layer.shadowColor = UIColor.clear.cgColor
+        context.previouslyFocusedView?.layer.shadowOpacity = 0
+        context.previouslyFocusedView?.layer.shadowOffset = CGSize.zero
+        context.previouslyFocusedView?.layer.shadowRadius = 0
+        context.previouslyFocusedView?.transform = CGAffineTransform.identity.scaledBy(x: 1.0, y: 1.0)
+    }
+    
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        
+        let nextFocusedView = String(describing: context.nextFocusedView)
+        let previouslyFocusedView = String(describing: context.previouslyFocusedView)
+        guard context.nextFocusedView != nil else {
+            return
+        }
+        
+        if previouslyFocusedView.contains("UITabBarButton") {
+           setPreferredFocus(view: playBtn)
+        }
+        
+        
+        if previouslyFocusedView.contains("UICollectionView") && nextFocusedView.contains("UITabBarButton") {
+           setPreferredFocus(view: playBtn)
+        }
+        
+        
+        
+        setNextFocusUI(context)
+        setPrevioulyFocusedUI(context)
+        //removeChildIfNead()
+    }
+    
+    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+        guard let focusView = preferredFocusView else { return [] }
+
+        return [focusView]
+    }
+
+    // Set the preferred focus view by value
+    func setPreferredFocus(view: UIView) {
+        preferredFocusView = view
+
+        setNeedsFocusUpdate()
+        updateFocusIfNeeded()
+    }
 }
